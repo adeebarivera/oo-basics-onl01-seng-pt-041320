72ad7e39bac1ba7e14523a14d4f2dd157ef7d991
adeeba reaz <adeebarivera@outlook.com>
2020-04-29 22:05:18 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,44 +1,22 @@
-# book.rb
- 
 class Book
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
+
   def initialize(title)
     @title = title
   end
- 
-  def title
-    @title
-  end
- 
-  def author=(author)
-    @author = author
-  end
- 
-  def author
-    @author
-  end
- 
-  def page_count=(num)
-    @page_count = num
-  end
- 
-  def page_count
-    @page_count
-  end
- 
-  def genre=(genre)
-    @genre = genre
-  end
- 
-  def genre
-    @genre
-  end
- 
+
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
- book = Book.new("Some Title")
- book.turn_page
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+    #or GENRES = @genre
+  end
 
 end
-
 
